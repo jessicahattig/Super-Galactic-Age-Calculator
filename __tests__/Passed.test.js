ac9f20312668fad51fd9c2bc4020e5a30d61239c
jessicahattig <jessicahattig@gmail.com>
2023-04-14 16:04:31 -0700

    WIP: Tests.

--- __tests__/Passed.test.js
+++ __tests__/Passed.test.js
@@ -10,12 +10,10 @@ describe('EarthYears', () => {
   test('should have 1 human year equal to 0.3023255814 Earth year.', () => {
     expect(yearspassed).toEqual(0.3023255814);
   });
+  test('should correctly convert 1 human year to Earth years', () => {
+    expect(yearspassed.EarthYears).toEqual(0.3023255814); 
+  });
 });
-  
-  // test('should correctly convert 1 human year to Earth years', () => {
-  //   expect(yearspassed.EarthYears).toEqual(0.3023255814); 
-  // });
-
 // 
 
 
