4724c004989239e2327ac998e7d6286920d02e6d
jessicahattig <jessicahattig@gmail.com>
2023-04-14 11:22:39 -0700

    WIP: Second test. Still working on galatic year multipliers. Taking 10 min break.

--- __tests__/Passed.test.js
+++ __tests__/Passed.test.js
@@ -13,17 +13,31 @@ describe('YearsPassed', () => {
 test('should correctly')
 
 //-------Galactic year multipliers-------
+//EARTH------------------------------------
 //43 human years / 13 Earth years = 3.3076923077 human years per Earth year
+//
+// 13 Earth years /43 human years = 0.3023255814 Earth years per human year
 
+//MERCURY--------------------------------
 // 43 human years / 54.16 Mercury years =
 // 0.79394387 huamn years per Mercury Year
+//
+// 54.16 Mercury Years / 43 human years = 1.2595348837 Mercury year per human year
 
+
+//VENUS---------------------------------
 // 43 human years / 20.96 Venus years = 2.0515267176 human years per Venus year.
+//
+// 20.96 Venus years / 43 human years = 0.4874418605 Venus year per human year
 
+//MARS-------------------------------------
 // 43 human years / 6.91 Mars years = 6.2228654124 human years per Mars year.
+//
+//6.91 Mars years/43 human years = 0.1606976744 Mars year per human year
 
+//Jupitar------------------------------------
 // 43 human years / 1.09 Jupiter years = 39.4495412844 human years per Jupitar year.
-
+//1.09 Jupitar years/ 43 human years = 0.0253488372 Jupitar year per human year
 
 
 
